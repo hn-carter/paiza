911dd421b5f7d3ccebef8c7905ab574838bf0999
hn-carter <84824787+hn-carter@users.noreply.github.com>
2021-12-18 10:34:13 +0900

    add binary search application step2 in swift

---
+++ binary_search/application_step2/swift/step2.swift
@@ -0,0 +1,55 @@
+/*
+これはpaizaラーニングの「二分探索メニュー」から「太巻きを分けよう (おなかペコペコ)」
+swiftでチャレンジしたコードです。
+https://paiza.jp/works/mondai/binary_search/binary_search__application_step2
+
+作成環境
+Ubuntu 20.04.2 LTS
+Swift version 5.4 (swift-5.4-RELEASE) Target: x86_64-unknown-linux-gnu
+*/
+import Foundation
+
+// メイン関数
+func main() {
+    // 1行目に、太巻きの長さ L と、分け合う人数 n と、切れ目の数 k が与えられます。
+    let items = readLine()!.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ")
+    let L = Int(items[0])!
+    let n = Int(items[1])!
+    let k = Int(items[2])!
+    // 2行目に、切れ目 A_i が半角スペース区切りで与えられます。
+    var a: [Int] = [Int](repeating: 0, count: k + 2)
+    let itemsA = readLine()!.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ")
+    for i in 0..<k {
+        a[i+1] = Int(itemsA[i])!
+    }
+    a[k+1] = L // 切れ目配列の最後に全体の長さをセット
+    // 答えを求める
+	var left: Int = 0
+	var right: Int = L + 1
+	var mid: Int = 0
+	while (right - left) > 1 {
+		mid = (left + right) / 2
+		// print(String(format: "left= %d right= %d mid= %d", left, right, mid))
+		// 切れ目配列の処理位置
+		var lastClack: Int = 0
+		// 分割出来た数
+		var num: Int = 0
+		for clack in a {
+			if clack - lastClack >= mid {
+				lastClack = clack
+				num += 1
+			}
+		}
+		if num < n {
+			// 長くて分けきれなかったので減らす
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	// 結果出力
+	print(left)
+}
+
+// エントリーポイント
+main()
