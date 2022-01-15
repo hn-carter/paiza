95d0cbb73e87a1bd1759ad5fd44e87961875fe27
hn-carter <84824787+hn-carter@users.noreply.github.com>
2022-01-15 21:47:05 +0900

    add dynamic programming primer partial sums step0 in swift

---
+++ dp_primer/partial_sums_step0/swift/step0.swift
@@ -0,0 +1,50 @@
+/*
+これはpaizaラーニングの「DPメニュー」から「部分和問題 1」
+swiftでチャレンジしたコードです。
+https://paiza.jp/works/mondai/dp_primer/dp_primer_partial_sums_step0
+
+作成環境
+Ubuntu 20.04.2 LTS
+Swift version 5.4 (swift-5.4-RELEASE) Target: x86_64-unknown-linux-gnu
+*/
+import Foundation
+
+// メイン関数
+func main() {
+    // 1行目に、おもりの個数 n と目標とする重さの和 x が半角スペース区切りで与えられます。
+    let items = readLine()!.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ")
+    let n = Int(items[0])!
+    let x = Int(items[1])!
+    // 続く n 行のうち i 行目では、おもり i の重さ a_i が与えられます。
+    var a: [Int] = [Int](repeating: 0, count: n)
+    for i in 0..<n {
+        a[i] = Int(readLine()!.trimmingCharacters(in: .whitespacesAndNewlines))!
+    }
+    // 動的計画法で漸化式を計算
+
+    // 重さを添字とした配列dpを初期化する
+    var dp: [Bool] = [Bool](repeating: false, count: x+1)
+    // おもりを選ばなければ、重さの和を0とすることができる
+    dp[0] = true
+    // おもり i までのおもりを使って
+    for i in 0..<n {
+        // 重さの和を j とすることができるか？
+        // 大きい方から逆にループしないと複数の重りを組み合わせたときに正しく計算できない
+        for j in (a[i]...x).reversed() {
+            // 配列が求めたい重さ j から 追加する重さ a_i を引いたときに
+            // true なら和を求めることができる
+            if (dp[j - a[i]]) {
+                dp[j] = true
+            }
+        }
+    }
+    // 結果出力
+    if dp[x] {
+        print("yes")
+    } else {
+        print("no")
+    }
+}
+
+// エントリーポイント
+main()
