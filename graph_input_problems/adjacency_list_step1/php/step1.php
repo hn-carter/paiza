714696250dbf846ff5ad4a73ee45c2057c09d163
hn-carter <84824787+hn-carter@users.noreply.github.com>
2022-02-20 10:03:08 +0900

    add graph input problems adjacency list step1 in php

---
+++ graph_input_problems/adjacency_list_step1/php/step1.php
@@ -0,0 +1,48 @@
+<?php
+/*
+これはpaizaラーニングの「グラフ構造の入力メニュー」-「隣接リストの出力・無向グラフ」に
+phpでチャレンジしたコードです。
+https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__adjacency_list_step1
+
+作成環境
+Ubuntu 20.04.2 LTS
+PHP 7.4.3 (cli) (built: Jul  5 2021 15:13:35) ( NTS )
+*/
+
+// ●エントリーポイントはこのファイルの最後です
+
+// メイン処理
+function main() {
+    // 1 行目に、頂点の個数を表す整数 n と、頂点の組の個数を表す整数 m が半角スペース区切りで与えられます。
+    $items = explode(" ", trim(fgets(STDIN)));
+    $n = $items[0];
+    $m = $items[1];
+    // 続く m 行では、頂点の組 a_i, b_i が半角スペース区切りで与えられます。(1 ≦ i ≦ m)
+    $vertex = array();
+    for ($i = 0; $i < $n; $i++) {
+        $vertex[$i] = array();
+    }
+    for ($i = 0; $i < $m; $i++) {
+        $itemsG = explode(" ", trim(fgets(STDIN)));
+        $vertex[$itemsG[0] - 1][] = $itemsG[1];
+        $vertex[$itemsG[1] - 1][] = $itemsG[0];
+    }
+    // 隣接リストの出力
+    for ($i = 0; $i < $n; $i++) {
+        if (count($vertex[$i]) == 0) {
+            echo "-1\n";
+        } else {
+            $max = count($vertex[$i]);
+            for ($j = 0; $j < $max; $j++) {
+                if ($j > 0) {
+                    echo " ";
+                }
+                echo $vertex[$i][$j];
+            }
+            echo "\n";
+        }
+    }
+}
+
+// エントリーポイント
+main();
