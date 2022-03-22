5500c4aadc904e774551160a70b0cf22375dcbf9
hn-carter <84824787+hn-carter@users.noreply.github.com>
2022-03-22 22:59:02 +0900

    add graph input problems degree euler graph step5 in golang

---
+++ graph_input_problems/degree_euler_graph_step5/golang/step5.go
@@ -0,0 +1,49 @@
+/*
+これはpaizaラーニングの「グラフ構造の入力メニュー」-「頂点の出現回数・有向グラフ」に
+go言語でチャレンジしたコードです。
+https://paiza.jp/works/mondai/graph_input_problems/graph_input_problems__degree_euler_graph_step5
+
+作成環境
+Ubuntu 20.04.2 LTS
+go version go1.16.4 linux/amd64
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// main はエントリーポイント
+func main() {
+	var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+	// 1 行目に、頂点の個数を表す整数 n と、頂点の組の個数を表す整数 m, 頂点 v が半角スペース区切りで与えられます。
+	sc.Scan()
+	var items []string = strings.Split(strings.TrimSpace(sc.Text()), " ")
+	//var n, _ = strconv.Atoi(items[0])
+	var m, _ = strconv.Atoi(items[1])
+	var v, _ = strconv.Atoi(items[2])
+	// 続く m 行では、頂点の組 a_i, b_i が半角スペース区切りで与えられます。(1 ≦ i ≦ m)
+	var indegree int = 0
+	var outdegree int = 0
+	for i := 0; i < m; i++ {
+		sc.Scan()
+		var items []string = strings.Split(strings.TrimSpace(sc.Text()), " ")
+		var a, _ = strconv.Atoi(items[0])
+		var b, _ = strconv.Atoi(items[1])
+		// 頂点につながる辺をカウント
+		if v == a {
+			outdegree++
+		}
+		if v == b {
+			indegree++
+		}
+	}
+	// 答えを出力する
+	fmt.Printf("%d %d\n", outdegree, indegree)
+	return
+}
